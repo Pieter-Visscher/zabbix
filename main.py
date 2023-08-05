290a1b125ad1aa24fb9d010438b43781eab580b3
Pieter Visscher | Bizway BV <p.visscher@bizway.nl>
2023-08-05 17:51:36 +0200

    testing api

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import os
 from dotenv import load_dotenv
-
+import requests
+import json
 load_dotenv()
-key=os.getenv("API_KEY")
+key=os.getenv("zabbix_key")
 
+ZABBIX_API_URL = "https://zabbix.pvisscher.nl/api_jsonrpc.php"
 
+#response = requests.get(
+#        'https://zabbix.pvisscher.nl/api_jsonrpc.php', headers={'Authorization': f'access_token {key}','Content-Type': 'application/json'}
+#        )
+r = requests.post(ZABBIX_API_URL,
+        json={
+            "jsonrpc": "2.0",
+            "method": "host.get",
+            "params": {
+                "filter": {
+                    "host": [
+                        "artoria",
+                        "debian"
+                        ]
+                    }
+                },
+            "id": 1,
+            "auth": f"{key}"
+            })
+
+print(json.dumps(r.json(), indent = 4, sort_keys=True))
 
