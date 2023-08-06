b1a9bf33a5991b568ec0e19e5b5104b1613e1798
Pieter Visscher | Bizway BV <p.visscher@bizway.nl>
2023-08-06 23:24:13 +0200

    fixed parsing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import requests
 import json
 load_dotenv()
+
 key=os.getenv("zabbix_key")
 ZABBIX_API_URL=os.getenv("api_url")
 
@@ -20,12 +21,9 @@ r = requests.post(ZABBIX_API_URL,
                 },
             "id": 1,
             "auth": f"{key}"
-            })
+            }
+        )
 
-#print(json.dumps(r.json(), indent = 4, sort_keys=True))
-#print(json.dumps(r.json([0]['result']["hostid"])))
+data = json.loads(json.dumps(r.json()))
 
-result = json.dumps(r.json())
-
-print(result[0]["result"]["hostid"])
-
+print(data['result'][0]['hostid'])
