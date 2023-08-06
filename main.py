479c53a8327e4d085fddbb6682d9eae7cd909430
Pieter Visscher | Bizway BV <p.visscher@bizway.nl>
2023-08-06 22:16:57 +0200

    removed url from file

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ import requests
 import json
 load_dotenv()
 key=os.getenv("zabbix_key")
+ZABBIX_API_URL=os.getenv("api_url")
 
-ZABBIX_API_URL = "https://zabbix.pvisscher.nl/api_jsonrpc.php"
-
-#response = requests.get(
-#        'https://zabbix.pvisscher.nl/api_jsonrpc.php', headers={'Authorization': f'access_token {key}','Content-Type': 'application/json'}
-#        )
 r = requests.post(ZABBIX_API_URL,
         json={
             "jsonrpc": "2.0",
@@ -26,5 +22,10 @@ r = requests.post(ZABBIX_API_URL,
             "auth": f"{key}"
             })
 
-print(json.dumps(r.json(), indent = 4, sort_keys=True))
+#print(json.dumps(r.json(), indent = 4, sort_keys=True))
+#print(json.dumps(r.json([0]['result']["hostid"])))
+
+result = json.dumps(r.json())
+
+print(result[0]["result"]["hostid"])
 
